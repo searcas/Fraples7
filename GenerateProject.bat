54c007cda738fa5eb7c3a2379b76c692c8b45a52
searcas <ultimatez1000@gmail.com>
2019-10-20 19:29:38 +0200

    Fix up

--- GenerateProject.bat
+++
@@ -1,2 +0,0 @@
-call vendor\ven\premake\premake5.exe vs2017
-PAUSE
--- scripts/GenerateProject.bat
+++ scripts/GenerateProject.bat
@@ -1,2 +1,5 @@
-call vendor\ven\premake\premake5.exe vs2017
+@echo off
+pushd %~dp0\..\
+call vendor\ven\premake\premake5.exe vs2019
+popd
 PAUSE
