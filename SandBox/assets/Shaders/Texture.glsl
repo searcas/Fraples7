671e8e72296cdf868ab64757caff76e9ebb283dc
Nikola Petrovic <ultimatez1000@gmail.com>
2021-11-08 14:13:14 +0100

    Branching Fragment Shader

--- SandBox/assets/Shaders/Texture.glsl
+++ SandBox/assets/Shaders/Texture.glsl
@@ -39,5 +39,41 @@
 
 	void main()
 	{	
-		color = texture(_uTextures[int(_vTexIndex)], _vTexCoord * _vTiling) * _vColor;
+		vec4 texColor = _vColor;
+		switch(int(_vTexIndex))
+		{
+			case 0: texColor *= texture(_uTextures[0], _vTexCoord * _vTiling); break;
+			case 1: texColor *= texture(_uTextures[1], _vTexCoord * _vTiling); break;
+			case 2: texColor *= texture(_uTextures[2], _vTexCoord * _vTiling); break;
+			case 3: texColor *= texture(_uTextures[3], _vTexCoord * _vTiling); break;
+			case 4: texColor *= texture(_uTextures[4], _vTexCoord * _vTiling); break;
+			case 5: texColor *= texture(_uTextures[5], _vTexCoord * _vTiling); break;
+			case 6: texColor *= texture(_uTextures[6], _vTexCoord * _vTiling); break;
+			case 7: texColor *= texture(_uTextures[7], _vTexCoord * _vTiling); break;
+			case 8: texColor *= texture(_uTextures[8], _vTexCoord * _vTiling); break;
+			case 9: texColor *= texture(_uTextures[9], _vTexCoord * _vTiling); break;
+			case 10: texColor *= texture(_uTextures[10], _vTexCoord * _vTiling); break;
+			case 11: texColor *= texture(_uTextures[11], _vTexCoord * _vTiling); break;
+			case 12: texColor *= texture(_uTextures[12], _vTexCoord * _vTiling); break;
+			case 13: texColor *= texture(_uTextures[13], _vTexCoord * _vTiling); break;
+			case 14: texColor *= texture(_uTextures[14], _vTexCoord * _vTiling); break;
+			case 15: texColor *= texture(_uTextures[15], _vTexCoord * _vTiling); break;
+			case 16: texColor *= texture(_uTextures[16], _vTexCoord * _vTiling); break;
+			case 17: texColor *= texture(_uTextures[17], _vTexCoord * _vTiling); break;
+			case 18: texColor *= texture(_uTextures[18], _vTexCoord * _vTiling); break;
+			case 19: texColor *= texture(_uTextures[19], _vTexCoord * _vTiling); break;
+			case 20: texColor *= texture(_uTextures[20], _vTexCoord * _vTiling); break;
+			case 21: texColor *= texture(_uTextures[21], _vTexCoord * _vTiling); break;
+			case 22: texColor *= texture(_uTextures[22], _vTexCoord * _vTiling); break;
+			case 23: texColor *= texture(_uTextures[23], _vTexCoord * _vTiling); break;
+			case 24: texColor *= texture(_uTextures[24], _vTexCoord * _vTiling); break;
+			case 25: texColor *= texture(_uTextures[25], _vTexCoord * _vTiling); break;
+			case 26: texColor *= texture(_uTextures[26], _vTexCoord * _vTiling); break;
+			case 27: texColor *= texture(_uTextures[27], _vTexCoord * _vTiling); break;
+			case 28: texColor *= texture(_uTextures[28], _vTexCoord * _vTiling); break;
+			case 29: texColor *= texture(_uTextures[29], _vTexCoord * _vTiling); break;
+			case 30: texColor *= texture(_uTextures[30], _vTexCoord * _vTiling); break;
+			case 31: texColor *= texture(_uTextures[31], _vTexCoord * _vTiling); break;
+		}
+		color = texColor;
 	}
